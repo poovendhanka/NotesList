557c1bfaf9d11cd17ee0569dc6a06a208b85b2da
poovendhan <poovendhanka@gmail.com>
2025-01-30 12:04:24 +0530

    Notes-app

--- src/main/java/com/notes/NotesList/Controller/TaskController.java
+++ src/main/java/com/notes/NotesList/Controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-//@RequestMapping("/tasks")
+@RequestMapping("/NotesList")
 public class TaskController {
 
     private final TaskService taskService;
